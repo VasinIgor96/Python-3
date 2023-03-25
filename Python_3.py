2142887e5e94d6b6fa38ef16ec5f57dc1f429f09
Vasin Igor <vasin.igor0796@gmail.net>
2023-03-25 14:54:11 +0200

    Ex_3 Task done

--- Python_3.py
+++ Python_3.py
@@ -1,1 +1,10 @@
 
+meters = int(input("Введіть кількість метрів: "))
+
+what = int(input("В що конвертувати метри? \n1. Милі \n2. Дюйми\n3. Ярда \nВиберіть варіант: "))
+if what == 1:
+   print(f"{meters} метрів = {meters*0.000621} миль")
+elif what == 2:
+   print(f"{meters} метрів = {meters*39.37} дюймів")
+elif what == 3:
+    print(f"{meters} метрів = {meters*1.093613} ярдів")
